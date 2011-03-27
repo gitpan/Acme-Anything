7d1d8fe92ecb3ddfdb0e9b35b9aeb567794a953c
Joshua ben Jore <jjore@cpan.org>
2011-03-27 17:24:23 +0000

    Import of JJORE/Acme-Anything-0.04 from CPAN.
    gitpan-cpan-distribution: Acme-Anything
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         JJORE/Acme-Anything-0.04.tar.gz
    gitpan-cpan-author:       JJORE
    gitpan-cpan-maturity:     released

--- lib/Acme/Anything.pm
+++ lib/Acme/Anything.pm
@@ -3,18 +3,17 @@ use strict;
 use 5.008;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
-push @INC, \ &fake_module;
+push @main::INC, \ &handler_of_last_resort;
 
-sub fake_module {
-    my $source_code = '1';
-    open my ($fh), '<', \ $source_code
-        or die "Can't open a psudeo-file: $!";
+sub handler_of_last_resort {
+    my $fake_source_code = '1';
+    open my ($fh), '<', \ $fake_source_code;
     return $fh;
-}
+};
 
-no warnings;    ## no critic
+no warnings;    ## no critic (warning)
 'Warning! The consumption of alcohol may cause you to think you have mystical kung-fu powers.'
 
 __END__
@@ -26,24 +25,48 @@ Acme::Anything - Anything, even imaginary modules are loadable
 =head1 SYNOPSIS
 
   use Acme::Anything;
-  use Fish; # Ok!
-  use CGI; # Also ok!
+  use Fish; # OK!
+  use CGI; # Also OK!
 
 =head1 DESCRIPTION
 
-This module inserts a hook into C<@INC> to load imaginary modules.
+This module inserts a hook into C<@INC> to load imaginary
+modules. Things that would ordinarily work continue to work. Things
+that would fail because the module doesn't exist won't fail anymore.
+
+=head1 INSTALLATION
+
+To install this module, run the following commands:
+
+    perl Makefile.PL
+    make
+    make test
+    make install
+
+=head1 SUPPORT AND DOCUMENTATION
+
+After installing, you can find documentation for this module with the
+perldoc command.
+
+    perldoc Acme::Anything
+
+You can also look for information at:
+
+=over
+
+=item RT, CPAN's request tracker L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-Anything>
+
+=item AnnoCPAN, Annotated CPAN documentation L<http://annocpan.org/dist/Acme-Anything>
+
+=item CPAN Ratings L<http://cpanratings.perl.org/d/Acme-Anything>
+
+=item Search CPAN L<http://search.cpan.org/dist/Acme-Anything>
+
+=back
 
 =head1 AUTHOR
 
-Josh ben Jore, C<< <jjore at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-C<bug-acme-anything at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-Anything>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
+Josh ben Jore, E<gt>jjore@cpan.orgE<lt>
 
 =head1 SUPPORT
 
@@ -53,7 +76,7 @@ You can find documentation for this module with the perldoc command.
 
 You can also look for information at:
 
-=over 4
+=over
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
@@ -73,11 +96,26 @@ L<http://search.cpan.org/dist/Acme-Anything>
 
 =back
 
-=head1 ACKNOWLEDGEMENTS
-
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2007 Josh ben Jore, all rights reserved.
+Copyright 2007, 2009, 2011 Joshua Jore
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+This program is distributed WITHOUT ANY WARRANTY, including but not
+limited to the implied warranties of merchantability or fitness for a
+particular purpose.
+
+The program is free software.  You may distribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation (either version 2 or any later version) and
+the Perl Artistic License as published by O’Reilly Media, Inc.  Please
+open the files named Copying and Artistic for a copy of these
+licenses.
+
+=begin emacs
+
+## Local Variables:
+## mode: pod
+## mode: auto-fill
+## End:
+
+=end emacs
